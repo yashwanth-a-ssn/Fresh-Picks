5464dc477625beb9337c278cb38f5437692a0742
Yashwanth A <yashwanth2025offl@gmail.com>
2026-03-26 19:25:15 +0530

    Backend Setup Testing

--- Backend-Setup-Testing/app.py
+++ Backend-Setup-Testing/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import subprocess
 
-app = Flask(__name__)
+app = Flask(__name__)   # ✅ FIRST define app
+CORS(app)               # ✅ THEN apply CORS
 
 @app.route('/test', methods=['GET'])
 def test():
